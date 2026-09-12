1dfa3b4b51b6b192926be4a1b49144952630f4d6
Ikeidjd <ikeidjd2@gmail.com>
2026-09-12 04:22:49 +0200

    Added the enemies to the world

--- src/world.cpp
+++ src/world.cpp
@@ -3,8 +3,25 @@
 #include <algorithm>
 
 #include "shapes/pseudo_triangle.hpp"
+#include "behaviors/chaser.hpp"
+#include "behaviors/charger.hpp"
 
-World::World(const sf::RenderWindow& window) : player(std::make_unique<shapes::PseudoTriangle>(static_cast<sf::Vector2f>(window.getSize()) * 0.5f, 40, 40, 0.25)) {}
+World::World(const sf::RenderWindow& window) : player(std::make_unique<shapes::PseudoTriangle>(static_cast<sf::Vector2f>(window.getSize()) * 0.5f, 40, 40, 0.25)) {
+	Enemy chaser(
+		std::make_unique<shapes::PseudoTriangle>(sf::Vector2f(), 30, 30, 0.25),
+		std::make_unique<behaviors::Chaser>(SmoothMover(900.0f, 300.0f, 300.0f)),
+		sf::Color::Red
+	);
+
+	Enemy charger(
+		std::make_unique<sf::CircleShape>(16.0f),
+		std::make_unique<behaviors::Charger>(SmoothMover(2200.0f, 800.0f, 800.0f)),
+		sf::Color::Blue
+	);
+
+    this->summon(std::move(chaser));
+    this->summon(std::move(charger));
+}
 
 void World::update(float delta_time, const sf::RenderWindow& window) {
     this->mouse_world_coords = window.mapPixelToCoords(sf::Mouse::getPosition(window));
